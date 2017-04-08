704f9095df89728b0460b901aff70f8d613aa7b5
Kamil Milewski <kamil.milewski@hotmail.com>
2017-04-08 17:30:11 +0200

    delete test folder - again...

--- test/controllers/quests_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class QuestsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get quests_index_url
-    assert_response :success
-  end
-end
